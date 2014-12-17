6f68deaa18c1060ae1cf0c39f1a5530a501096a3
Dennis Crissman <dcrissma@redhat.com>
2014-12-17 09:55:44 -0500

    work on unit tests

--- lightblue-ldap-integration-test/src/test/java/com/redhat/lightblue/crud/ldap/ITCaseLdapCRUDControllerTest.java
+++ lightblue-ldap-integration-test/src/test/java/com/redhat/lightblue/crud/ldap/ITCaseLdapCRUDControllerTest.java
@@ -19,21 +19,27 @@
 package com.redhat.lightblue.crud.ldap;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import com.redhat.lightblue.DataError;
+import com.redhat.lightblue.Response;
 import com.redhat.lightblue.config.DataSourcesConfiguration;
+import com.redhat.lightblue.config.JsonTranslator;
 import com.redhat.lightblue.config.LightblueFactory;
+import com.redhat.lightblue.crud.InsertionRequest;
 import com.redhat.lightblue.ldap.test.LdapServerExternalResource;
 import com.redhat.lightblue.ldap.test.LdapServerExternalResource.InMemoryLdapServer;
-import com.redhat.lightblue.mediator.Mediator;
 import com.redhat.lightblue.mongo.test.MongoServerExternalResource;
 import com.redhat.lightblue.mongo.test.MongoServerExternalResource.InMemoryMongoServer;
+import com.redhat.lightblue.util.Error;
+import com.redhat.lightblue.util.JsonUtils;
 import com.redhat.lightblue.util.test.AbstractJsonNodeTest;
 
 @InMemoryLdapServer
@@ -47,25 +53,58 @@ public class ITCaseLdapCRUDControllerTest{
     public MongoServerExternalResource mongoServer = new MongoServerExternalResource();
 
     @Before
-    public void before() throws IOException, ClassNotFoundException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException{
-        if(mediator == null){
-            LightblueFactory factory = new LightblueFactory(
-                    new DataSourcesConfiguration(AbstractJsonNodeTest.loadJsonNode("./datasources.json")));
-            mediator = factory.getMediator();
-        }
+    public void before() throws IOException {
+        lightblueFactory = new LightblueFactory(
+                new DataSourcesConfiguration(AbstractJsonNodeTest.loadJsonNode("./datasources.json")));
+    }
+
+    @After
+    public void after(){
+        lightblueFactory = null;
     }
 
     public ITCaseLdapCRUDControllerTest(){
         System.setProperty("ldap.host", "localhost");
         System.setProperty("ldap.port", String.valueOf(LdapServerExternalResource.DEFAULT_PORT));
+
+        System.setProperty("mongo.host", "localhost");
+        System.setProperty("mongo.port", String.valueOf(MongoServerExternalResource.DEFAULT_PORT));
     }
 
-    public Mediator mediator;
+    public LightblueFactory lightblueFactory;
 
     @Test
-    public void test(){
-        assertNotNull(mediator);
+    public void testInsert() throws Exception{
+        JsonTranslator tx = lightblueFactory.getJsonTranslator();
+        InsertionRequest insertIequest = tx.parse(InsertionRequest.class, JsonUtils.json("{}"));
+        Response response = lightblueFactory.getMediator().insert(insertIequest);
+
+        assertNotNull(response);
+        //assertNoErrors(response);
     }
 
+    private void assertNoErrors(Response response){
+        if(response.getErrors().isEmpty() && response.getDataErrors().isEmpty()){
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        if(!response.getErrors().isEmpty()){
+            builder.append("Response has Errors: \n");
+            for(Error error : response.getErrors()){
+                builder.append(error.toJson()+ "\n");
+            }
+        }
+
+        if(!response.getDataErrors().isEmpty()){
+            builder.append("Response has Data Errors: \n");
+            for(DataError error : response.getDataErrors()){
+                builder.append(error.toJson() + "\n");
+            }
+        }
+
+        fail(builder.toString());
+    }
 
 }
