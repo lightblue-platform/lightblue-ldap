20dba07d249c0a5fcdce59c704bc9d9701346cc7
Dennis Crissman <dcrissma@redhat.com>
2015-01-27 10:47:07 -0500

    fix crud errors so that they actually are sent to the client

--- lightblue-ldap-crud/src/main/java/com/redhat/lightblue/crud/ldap/LdapCRUDController.java
+++ lightblue-ldap-crud/src/main/java/com/redhat/lightblue/crud/ldap/LdapCRUDController.java
@@ -235,9 +235,7 @@ public class LdapCRUDController implements CRUDController{
                     translatedDocs.add(resultTranslator.translate(entry));
                 }
                 catch(Exception e){
-                    DocCtx erroredDoc = new DocCtx(null);
-                    erroredDoc.addError(Error.get(e));
-                    translatedDocs.add(erroredDoc);
+                    ctx.addError(Error.get(e));
                 }
             }
             ctx.setDocuments(translatedDocs);
